2b59681b7b82dc1a45d2e6fb4d9a1d0438ef0058
Parker Rudd <parkerbrudd@gmail.com>
2022-06-21 16:49:58 -0600

    started server and api creation

---
+++ api/server.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
+    .then(() => console.log('Connected to DB'))
+    .catch(console.error)
+
+const Todo = require('./models/Todo'); 
+
+app.get('/todos', async (req, res) => {
+    const todos = await Todo.find()
+
+    res.json(todos)
+})
+
+app.listen(30001, () => console.log('Server started on port 3001'))
