92d17d1c20fa6545ff121a02930b0347851457aa
Parker Rudd <parkerbrudd@gmail.com>
2022-06-25 10:52:53 -0600

    created api and client side

--- api/server.js
+++ api/server.js
@@ -24,4 +24,30 @@ app.get('/todos', async (req, res) => {
     res.json(todos)
 })
 
-app.listen(30001, () => console.log('Server started on port 3001'))
+app.post('/todo/new', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    }); 
+
+    todo.save()
+
+    res.json(todo)
+})
+
+app.delete('/todo/delete/:id', async (req, res) => {
+    const result = await Todo.findByIdAndDelete(req.params.id)
+
+    res.json(result)
+})
+
+app.put('/todo/complete/:id', async (req, res) => {
+    const todo = await Todo.findById(req.params.id)
+
+    todo.complete = !todo.complete
+
+    todo.save()
+    
+    res.json(todo)
+})
+
+app.listen(3001, () => console.log('Server started on port 3001'))
